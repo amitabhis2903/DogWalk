0a12374895d9bbcbb3da871a17fc4e18a983be7e
Amitabh <amitabhis2903@gmail.com>
2019-05-25 11:38:19 +0530

    initial commit

--- DogWalk/ViewController.swift
+++ DogWalk/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    let identifier: String = "Cell"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
     }
 
-
+    @IBAction func add(_ sender: Any) {
+    }
+    
 }
 
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
+
+extension ViewController: UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    
+}
